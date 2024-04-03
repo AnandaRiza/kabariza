d2f8471b732fef4fa7d9b476a241e5f22dc74dd9
ananda.riza <rizapratomo@graduate.utm.my>
2024-04-03 20:47:47 +0700

    minor changes (ketinggalan)

--- app/src/main/java/com/example/kabariza/presentation/home_screen/view/HomeActivity.kt
+++ app/src/main/java/com/example/kabariza/presentation/home_screen/view/HomeActivity.kt
@@ -2,8 +2,12 @@ package com.example.kabariza.presentation.home_screen.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.viewpager2.widget.ViewPager2
 import com.example.kabariza.R
 import com.example.kabariza.databinding.ActivityHomeBinding
@@ -16,30 +20,37 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var loadingProgressBar: ProgressBar
     private val onNavigationItemSelectedListener =
         BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.action_sport -> {
-
-                    // Show the Sport fragment
+                    loadingProgressBar.visibility = View.VISIBLE // Show loading progress bar
                     replaceFragment(NewsSportFragment())
                     return@OnNavigationItemSelectedListener true
                 }
+
                 R.id.action_health -> {
-                    replaceFragment(HealthFragment())// Show the Health fragment
+                    loadingProgressBar.visibility = View.VISIBLE // Show loading progress bar
+                    replaceFragment(HealthFragment())
                     return@OnNavigationItemSelectedListener true
                 }
+
                 R.id.action_technology -> {
-                    replaceFragment(TechFragment()) // Show the Technology fragment
+                    loadingProgressBar.visibility = View.VISIBLE // Show loading progress bar
+                    replaceFragment(TechFragment())
                     return@OnNavigationItemSelectedListener true
                 }
+
                 else -> false
             }
         }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loadingProgressBar = binding.loadingProgressBar
 //        replaceFragment(HomeFragment())
         binding.bottomNavigationViewact.setOnNavigationItemSelectedListener(
             onNavigationItemSelectedListener
@@ -69,7 +80,19 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.Fragment_Container, fragment).commit()
+        loadingProgressBar.visibility = View.VISIBLE
+
+        supportFragmentManager.beginTransaction().replace(R.id.Fragment_Container, fragment)
+            .commit()
+
+        fragment.viewLifecycleOwnerLiveData.observe(this, { viewLifecycleOwner ->
+            viewLifecycleOwner?.lifecycle?.addObserver(object : LifecycleObserver {
+                @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                fun onDestroy() {
+                    loadingProgressBar.visibility = View.GONE
+                }
+            })
+        })
     }
 
 
